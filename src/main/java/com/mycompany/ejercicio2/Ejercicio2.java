3e3859f3b31c25f020e865668c14c42afe5ddb1f
Kelvin Dariel Muñoz Muñoz <kelvinmunos783@gmail.com>
2023-11-06 17:37:59 -0600

    Primer subida de eeste ejercicio

---
+++ src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.ejercicio2;
+
+/**
+ *
+ * @author kelvi
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 3;
+        
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("División: " + (num1 / num2));
+        System.out.println("Módulo: " + (num1 % num2));
+    }
+}
+   
